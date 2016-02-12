8b257a592e2306b568a3cb02a67c5c4b698f7417
Niklas Klein <mail@taig.io>
2016-02-12 12:24:58 +0100

    Enforce java 7 compilation

--- project/Settings.scala
+++ project/Settings.scala
@@ -4,8 +4,16 @@ import sbt.Keys._
 
 object Settings {
     val common = Seq(
+        javacOptions ++=
+            "-source" :: "1.7" ::
+            "-target" :: "1.7" ::
+            Nil,
         organization := "io.taig.android.viewvalue",
         scalaVersion := "2.11.7",
+        scalacOptions ++=
+            "-feature" ::
+            "-deprecation" ::
+            Nil,
         githubProject := "viewvalue"
     )
     
